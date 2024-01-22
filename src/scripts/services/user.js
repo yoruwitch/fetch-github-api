b1d3b052d20cea990b1b16d0c6be93fdfb7c93ea
yoruwitch <evelyn.fernandes4@gmail.com>
2024-01-22 16:06:20 -0300

    Updated user.js from services settings

--- src/scripts/services/user.js
+++ src/scripts/services/user.js
@@ -2,7 +2,6 @@ import { baseUrl } from "../variables.js";
 
 async function searchUser(userName) {
     const response = await fetch(`${baseUrl}/${userName}`);
-
     return await response.json();
 }
 
